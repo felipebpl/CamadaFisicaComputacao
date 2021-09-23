73017aaf0c3b010afb32edd9fb2b909a5521e7ef
Felipe Lemos <felipebpl@al.insper.edu.br>
2021-09-23 14:38:11 -0300

    criando o Head

--- classes.py
+++ classes.py
@@ -13,26 +13,53 @@ class Datagram:
         self.com1 = enlace(door)
         self.com1.enable()
         print('open')
-        self.eop = b'\xaa\xaa\xaa\xaa'
+        self.eop = b'\xFF\xAA\xFF\xAA'
 
     def create_datagram(self, head, pkg=b''):
         return (head + pkg + self.eop)
 
-class Head():
-    def __init__(self, total, size, pkg_number):
+# h0 – tipo de mensagem
+# h1 – id do sensor
+# h2 – id do servidor
+# h3 – número total de pacotes do arquivo
+# h4 – número do pacote sendo enviado
+# h5 – se tipo for handshake:id do arquivo
+# h5 – se tipo for dados: tamanho do payload
+# h6 – pacote solicitado para recomeço quando a erro no envio.
+# h7 – último pacote recebido com sucesso.
+# h8 – h9 – CRC
+# PAYLOAD – variável entre 0 e 114 bytes. Reservado à transmissão dos arquivos.
+# EOP – 4 bytes: 0xFF 0xAA 0xFF 0xAA
 
-        self.head_list = []
-        self.total = total
-        self.size = size
+class Head():
+    def __init__(self, type, id_sensor, id_server, total, pkg_number, payload_size, pacote_solicitado, last_pkg, CRC8, CRC9):
+        self.msg_type = type
+        self.sensor_id = id_sensor
+        self.server_id = id_server
+        self.total_pkg = total
         self.pkg_number = pkg_number
+        if self.msg_type == 3:
+            self.size = payload_size
+        else:
+            self.size = b'\x00'
+        self.pkg_solicitado = pacote_solicitado
+        self.last_pkg = last_pkg
+        self.crc8 = CRC8
+        self.crc9 = CRC9
+        self.head_list = []
     
     def create_head(self):
-        #head_list.append((self.type).to_bytes(1, 'big'))
-        self.head_list.append(int(self.total).to_bytes(1, 'big'))
-        self.head_list.append(int(self.size).to_bytes(1, 'big'))
+        self.head_list.append(int(self.msg_type).to_bytes(1, 'big'))
+        self.head_list.append(int(self.sensor_id).to_bytes(1, 'big'))
+        self.head_list.append(int(self.server_id).to_bytes(1, 'big'))
+        self.head_list.append(int(self.total_pkg).to_bytes(1, 'big'))
         self.head_list.append(int(self.pkg_number).to_bytes(1, 'big'))
+        self.head_list.append(int(self.size).to_bytes(1, 'big'))
+        self.head_list.append(int(self.pkg_solicitado).to_bytes(1, 'big'))
+        self.head_list.append(int(self.last_pkg).to_bytes(1, 'big'))
+        self.head_list.append(int(self.crc8).to_bytes(1, 'big'))
+        self.head_list.append(int(self.crc9).to_bytes(1, 'big'))
         self.head = b''.join(self.head_list)
-
         while len(self.head) != 10:
             self.head += b'\x00'
 
@@ -67,3 +94,4 @@ class Payload():
         for size in range(len(self.package_size())):
             self.n.append(size + 1)
         return self.n
+
